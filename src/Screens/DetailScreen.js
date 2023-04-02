397d6aa974519fba627dab5cf739fb9e078fb7ab
dannertech <danner@danner.tech>
2023-04-02 15:56:28 -0400

    initial files plus coded in navigation and created components directory, added SearchScreen and DetailScreen

---
+++ src/Screens/DetailScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const DetailScreen = () => {
+    return(
+        <View>
+            <Text>This is the Detail Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+});
+
+export default DetailScreen;
---
+++ src/Screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const SearchScreen = () => {
+    return(
+        <View>
+            <Text>This is the Search Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
