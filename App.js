a44d75a8efe84c08678e3d99c3f08fbb7ca7e75b
dannertech <danner@danner.tech>
2023-04-02 21:13:58 -0400

    had to remove dotenv'

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SearchScreen from './src/Screens/SearchScreen';
 import DetailScreen from './src/Screens/DetailScreen';
+
 
 const Stack = createNativeStackNavigator();
 
--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import SearchBar from '../Components/SearchBar';
+import axios from 'axios';
+
+
+async function getData(){
+    await axios.get('https://api.yelp.com/v3/businesses/search', {
+        headers: {
+            'Authorization': 'Bearer XTC1RAyu6yEe8O_mWzXy1T6BRp30m1IMT_Jk_zlPu1YmUeVAMgOppKIcfYONPWS5XdEf-EQ657dFWLYzL2R6ylA3FPyYQM_XzhetjDOpK59vUVGtoVCjbAhbYH4cYXYx'
+        },
+        params: {
+            location: 'atlanta',
+            term: 'sushi'
+        }
+    }).then((data) => console.log(data)).catch((error) => console.log(error))
+}
 
 const SearchScreen = () => {
+    useEffect(() => {
+       getData()
+    })
     return(
         <View>
             <SearchBar />
